907664fd6427d26f75d030d4e52c36f7f8715e64
Kenny Su <ksu902446@gmail.com>
2022-01-13 15:27:11 -0500

    added store, nav reducer

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { Provider } from 'react-redux';
+import store from './store';
 
 export default function App() {
   return (
