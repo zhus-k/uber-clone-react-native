d61b076ada8fd7e8216b0702e6ff0401ade7cee0
Kenny Su <ksu902446@gmail.com>
2022-01-13 16:12:07 -0500

    updated homescreen
    added image and styled in tw

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 import tw from 'twrnc';
 
 
 const HomeScreen = () => {
     return (
         <SafeAreaView style={tw`bg-white h-full`}>
-            <Text style={tw`text-red-500 p-10`}>I am the Home Screen</Text>
+            <View style={tw`p-5`}>
+                <Image
+                    style={{
+                        width: 100,
+                        height: 100,
+                        resizeMode: 'contain',
+                    }}
+                    source={{ uri: 'https://links.papareact.com/gzs' }}
+                />
+            </View>
         </SafeAreaView>
     );
 };
