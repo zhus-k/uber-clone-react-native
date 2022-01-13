3a1e57c842e8691905d334e415cf73567705b374
Kenny Su <ksu902446@gmail.com>
2022-01-13 15:08:34 -0500

    Add react redux

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Provider } from 'react-redux';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>I will build a clone of Uber!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>I will build a clone of Uber!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
