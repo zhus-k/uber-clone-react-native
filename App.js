d7b5b50e2246c5a44de5194ecd207564d95b4144
Kenny Su <ksu902446@gmail.com>
2022-01-13 17:14:29 -0500

    Add nav options with navigation
    Added NavOption
    Added React Native Elements (reusable UI Components)
    Added React Navigation + Stack
    uses useNavigation Hook
    Added Map Screen

--- App.js
+++ App.js
@@ -4,11 +4,42 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
 import HomeScreen from './screens/HomeScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MapScreen from './screens/MapScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <Provider store={store}>
-      <HomeScreen />
+      <NavigationContainer>
+        <SafeAreaProvider>
+
+          <Stack.Navigator>
+
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+
+            <Stack.Screen
+              name="MapScreen"
+              component={MapScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+
+          </Stack.Navigator>
+
+        </SafeAreaProvider>
+
+      </ NavigationContainer>
     </Provider>
   );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 import tw from 'twrnc';
+import NavOptions from '../components/NavOptions';
 
 
 const HomeScreen = () => {
     return (
         <SafeAreaView style={tw`bg-white h-full`}>
             <View style={tw`p-5`}>
+
                 <Image
                     style={{
                         width: 100,
@@ -15,6 +17,9 @@ const HomeScreen = () => {
                     }}
                     source={{ uri: 'https://links.papareact.com/gzs' }}
                 />
+
+                <NavOptions />
+
             </View>
         </SafeAreaView>
     );
